0faecbc5bc58d99aeddbde411755811be2271e78
Baran Koç <19240000971@ogrenci.ege.edu.tr>
2026-04-18 19:48:45 +0300

    aman ammaz

--- merhaba.py
+++ merhaba.py
@@ -1,7 +1,11 @@
 def asal_sayi_mi(x): 
     for i in range(2, int(x**0.5) + 1): 
         if x % i == 0:
+            print("asal degildir")
             break
     else:
         print("asaldir")
-asal_sayi_mi(13)
+def asal_ayikla(x):
+    if x == 2: "print asaldir."
+    if x % 2 != 0: asal_sayi_mi(x)
+asal_ayikla(4)
